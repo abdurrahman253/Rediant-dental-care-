75b5b3c15053148b50a0fdb031263326585fc68c
Abdur Rahman <abdur@example.com>
2026-04-25 10:37:49 +0600

    fix: tailwind and babel config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    forceSwcTransforms: false,
+  },
   images: {
     remotePatterns: [
       {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-  ],
+ content: [
+  './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+  './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+],
   theme: {
     extend: {
       colors: {
